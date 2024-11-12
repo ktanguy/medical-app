eeeaa42f2935980d035b8587b0730a0b4c1c1044
ktanguy <t.kwizera@alustudent.com>
2024-11-12 22:18:03 +0200

    initial commit

--- Medical/medical-app/app/page.tsx
+++ Medical/medical-app/app/page.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 
-const page = () => {
+export default function Home() {
   return (
-    <div>
+    
       <section className='text-3xl flex min-h-screen justify-center items-center'>
         <h2 className='font-semibold'>Medical App</h2>
       </section>
-    </div>
-  )
+    
+  );
 }
-
-export default page
