07bd737ec6360aed7a86b03e6e56894028752fc6
ktanguy <t.kwizera@alustudent.com>
2024-11-12 14:16:36 +0200

    web app commit

---
+++ Medical/medical-app/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <section className='text-3xl flex min-h-screen justify-center items-center'>
+        <h2 className='font-semibold'>Medical App</h2>
+      </section>
+    </div>
+  )
+}
+
+export default page
